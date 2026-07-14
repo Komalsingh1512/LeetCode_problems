172461fcc2172025720d6fe75ab0d00b8fe969ae
Komal Singh <115030881+Komalsingh1512@users.noreply.github.com>
2026-07-15 01:04:34 +0530

    Time: 0 ms (100%), Space: 20.4 MB (83.25%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
@@ -6,10 +6,10 @@ public:
 
         for(int i = 0; i < n; i++) {
             nums[i] = nums[i] + n * (nums[nums[i]] % n);
-        }
+        } // firstly we need to calculate this, and the value that will come will update its previsous value... so to calcuate nums[5] we need to write the updated value not the previous one 
 
         for(int i = 0; i < n; i++) {
-            nums[i] = nums[i] / n;
+            nums[i] = nums[i] / n;// after the first loop we will run this loop and the result we will get is the final result 
         }
 
         return nums;
