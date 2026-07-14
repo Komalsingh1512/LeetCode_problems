eb3c8ad0b7c06f74bc9b1ecc658f5cbfc2558b45
Komal Singh <115030881+Komalsingh1512@users.noreply.github.com>
2026-07-15 01:21:05 +0530

    Time: 0 ms (100%), Space: 12.5 MB (82.05%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -4,9 +4,9 @@ public:
         vector<int> runningSum(nums.size());
        for(int i=0; i<nums.size(); i++){
         if(i==0){
-            runningSum[i]= nums[i];
+            runningSum[i]= nums[i];//if there is only single value then the sum will be that number only 
         }else{
-            runningSum[i]=runningSum[i-1]+ nums[i];
+            runningSum[i]=runningSum[i-1]+ nums[i];// every running sum will update its previous value so when we will calculate runningSum[2] we will take the updated value not the previous one 
             }
        } 
        return {runningSum};
