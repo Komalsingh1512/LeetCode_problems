2726d0dcbcdec3d22cdd2c1eb0fcf6ed5300baf4
Komal Singh <115030881+Komalsingh1512@users.noreply.github.com>
2026-07-22 12:53:30 +0530

    Time: 3 ms (13.72%), Space: 15.8 MB (5.87%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,43 +1,41 @@
 class Solution {
 public:
+// done using dfs... 
+    void dfs(int node, vector<int> &visited, vector<vector<int>> &adj) {
+
+        visited[node] = 1;
+
+        for (int adjNode : adj[node]) {
+            if (!visited[adjNode]) {
+                dfs(adjNode, visited, adj);
+            }
+        }
+    }
+
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
 
         int v = rooms.size();
-        vector<vector<int>> adj(v);
 
         // Create adjacency list
+        vector<vector<int>> adj(v);
+
         for (int i = 0; i < v; i++) {
             for (int j = 0; j < rooms[i].size(); j++) {
                 int b = rooms[i][j];
-                adj[i].push_back(b);     // changed
+                adj[i].push_back(b);
             }
         }
 
         vector<int> visited(v, 0);
 
-        queue<int> q;
+        // Start DFS from room 0
+        dfs(0, visited, adj);
 
-        int start = 0;
-        q.push(start);
-        visited[start] = 1;
-
-        while (!q.empty()) {
-
-            int node = q.front();
-            q.pop();
-
-            for (int adjNode : adj[node]) {
-
-                if (!visited[adjNode]) {
-                    visited[adjNode] = 1;
-                    q.push(adjNode);
-                }
-            }
-        }
-
+        // Check if all rooms are visited
         for (int i = 0; i < v; i++) {
-            if (!visited[i])
+            if (!visited[i]) {
                 return false;
+            }
         }
 
         return true;
