dc2d4fd9b068b95e1806c860328bf32f330c5633
Komal Singh <115030881+Komalsingh1512@users.noreply.github.com>
2026-07-30 21:27:12 +0530

    Time: 23 ms (5.95%), Space: 24.2 MB (7.51%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans = 0;
-
-        for(int i = 0; i < nums.size(); i++) {
-            ans = ans ^ nums[i];// it will give give me the xor value 
+      set<int>st;
+      for(auto num: nums){
+        if(st.find(num) ==st.end())// this lines say is the lement is not in the list then only insert 
+        st.insert(num);
+        else{
+            st.erase(num);
         }
-
-        return ans;
+      }
+      return *st.begin();
     }
 };
+//basixally we are creating a set and we are storing the element in the set which is not there right now, and when the same element is in the list then insteed of storing we are removing the same element so after all the insertion deletion the single element will left and at last we are printing that last set element 
