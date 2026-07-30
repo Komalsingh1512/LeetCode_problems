fe30de76226112ced15f61990e66a61ca4e8e7a9
Komal Singh <115030881+Komalsingh1512@users.noreply.github.com>
2026-07-30 21:33:35 +0530

    Time: 153 ms (5.01%), Space: 113.7 MB (5.99%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-
-        unordered_set<int> st;
-
-        for(int num : nums) {//ye ek ek karke sare elements ko lega 
-
-            if(st.find(num) != st.end())// ye check kar raha kya ye element stack me hai?? aagar hai to true return karo aagar nhi hai to insert karo set me 
-                return true;
-
-            st.insert(num);
+        set<int>st;
+        for(auto num: nums){
+            if(st.find(num) ==st.end()){
+                st.insert(num);// this line is just adding the number in the set if it in not there 
+            }
+            else{
+                return true;// if number is there return true
+            }
         }
-
-        return false;
+        return false;//or return false
     }
 };
