4ff16e068bed25196db0e72f2bf1e8fa1ac76175
Komal Singh <115030881+Komalsingh1512@users.noreply.github.com>
2026-07-31 20:09:13 +0530

    Time: 0 ms (100%), Space: 10.6 MB (44.86%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -2,43 +2,43 @@ class Solution {
 public:
     string reverseWords(string s) {
 
-        vector<string> words;
+        stack<string> st;
         string temp = "";
 
-        // Step 1: Extract words
-        for (char c : s) {
+        // Extract words and push into stack
+        for(char c : s){
 
-            if (c != ' ') {
-                temp += c;
+            if(c != ' '){
+                temp += c;// it will store first word in the temp
             }
-            else {
+            else{
 
-                if (!temp.empty()) {
-                    words.push_back(temp);
+                if(!temp.empty()){// when the space comes it temp value will be stored in stack and temp become null
+                    st.push(temp);
                     temp = "";
                 }
             }
         }
 
-        // Last word
-        if (!temp.empty()) {
-            words.push_back(temp);
+        // Push last word
+        if(!temp.empty()){// at last the last word is stored in temp not in stack because it is not have space at last that why we are adding it here in stack
+            st.push(temp);
         }
 
-        // Step 2: Reverse the vector
-        reverse(words.begin(), words.end());
-
-        // Step 3: Join the words
+        // Pop and build answer
         string ans = "";
 
-        for (int i = 0; i < words.size(); i++) {
+        while(!st.empty()){
 
-            ans += words[i];
+            ans += st.top();
+            st.pop();
 
-            if (i != words.size() - 1)
+            if(!st.empty()){
                 ans += " ";
+            }
         }
 
         return ans;
     }
 };
+//The stack solution works on the Last In, First Out (LIFO) principle. First, we traverse the given string character by character and create each word using a temporary string (temp). Whenever we encounter a space, we check if temp is not empty. If it contains a word, we push that word into the stack and clear temp to start forming the next word. This also helps us ignore multiple consecutive spaces because we only push non-empty words. After the traversal is complete, the last word may still be stored in temp (since the string might not end with a space), so we push it into the stack as well.
