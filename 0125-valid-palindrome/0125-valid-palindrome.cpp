11bfc4f322473e3f01b8d6d6aa5c195fea474650
Komal Singh <115030881+Komalsingh1512@users.noreply.github.com>
2026-07-31 19:39:38 +0530

    Time: 0 ms (100%), Space: 10 MB (54.65%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
- bool isValid(char ch){
-                if((ch >='a' && ch <='z') ||(ch >='A' && ch <='Z') || (ch >='0' && ch <='9')){
-                    return true;
-                }
-                return false;
-            }
+
+    bool solve(string &s, int left, int right){
+
+        // Base Case
+        if(left >= right)
+            return true;
+
+        // Skip non-alphanumeric characters
+        if(!isalnum(s[left]))
+            return solve(s, left + 1, right);
+
+        if(!isalnum(s[right]))
+            return solve(s, left, right - 1);
+
+        // Compare ignoring case
+        if(tolower(s[left]) != tolower(s[right]))
+            return false;
+
+        return solve(s, left + 1, right - 1);
+    }
+
     bool isPalindrome(string s) {
-        int left=0; 
-        int n= s.length();
-        int right=n-1;
-        bool palindrom = true;
-       
-        while(left < right){
-           
-            if(!isValid(s[left])){//it check if th element is space or other than letters
-                left++;
-            }
-            else if(!isValid(s[right])){
-                right--;
-            }else{
-                if(tolower(s[left]) != tolower(s[right])){
-                     return false;
-                }
-                else{
-                    left++;
-                    right--;
-                }
-            }
-        }
-    return true;
+
+        return solve(s,0,s.size()-1);
+
     }
 };
