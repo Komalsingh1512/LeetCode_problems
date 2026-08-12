f05b33b47097987a97aab0b8cf7efcff611f7cc5
Komal Singh <115030881+Komalsingh1512@users.noreply.github.com>
2026-08-12 15:42:11 +0530

    Time: 0 ms (100%), Space: 11.6 MB (79.67%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -6,7 +6,7 @@ public:
 
         for(int i = 0; i < n - 1; i++) {
 
-            for(int j = 0; j < n - i - 1; j++) {
+            for(int j = 0; j < n - i - 1; j++) {// this line will just short the next comming element it is not sorting the previous one so for that we are using for loop so that j loop is check each iteration of i 
 
                 if(nums[j] > nums[j + 1]) {
                     swap(nums[j], nums[j + 1]);
